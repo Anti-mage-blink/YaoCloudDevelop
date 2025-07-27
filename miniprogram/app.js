4ee4a85a92ed65d6dc5422791ef32eea435b45e2
Anti-mage-blink <1178801026@qq.com>
2025-07-28 06:53:58 +0800

    删除原云开发示例文件前备份，当前处于刚把FileIDList后端push到流水线

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -7,6 +7,7 @@ App({
     // 用户信息，先采用默认值，后续查询传入
     nickname: "默认昵称",
     avatar_fileid: "cloud://cloud1-5gbsnpgt113642bc.636c-cloud1-5gbsnpgt113642bc-1368918426/通用图像文件/默认头像.png",
+    openidPromise: null,
   },
 
   onLaunch() {
@@ -24,45 +25,47 @@ App({
     }
 
     // 获取openid-创建用户-查询用户信息
-    wx.cloud.callFunction({
-      name: "getOpenId",
+    this.globalData.openidPromise = new Promise((resolve, reject) => {
+      wx.cloud.callFunction({
+        name: "getOpenId",
 
-      success: res => {
-        const openid = res.result;
-        console.log("openid: " + openid);
-        this.globalData.openid = openid;
-
-        // 创建用户（保证用户已创建）
-        const data_createUser = {
-          "openid": openid,
-        };
-        this.callContainer("/api/v1/users", "POST", data_createUser)
-          .then( () => {
-            // console.log("用户创建成功");
-            // 查询用户信息
-            const data_getUserInfo = {
-              "openid": this.globalData.openid,
-            };
-            this.callContainer("/api/v1/users", "GET", data_getUserInfo)
-              .then(data => {
-                // console.log("用户信息查询成功");
-                // console.log(data);
-                // 获取后，若有效，更新用户信息（全局变量）
-                // （后续迭代：尽量从本地获取，减少请求量）
-                if (data.nickname != "") {
-                  this.globalData.nickname = data.nickname;
-                }
-                if (data.avatar_fileid != "") {
-                  this.globalData.avatar_fileid = data.avatar_fileid;
-                }
-                // console.log(this.globalData.nickname);
-                // console.log(this.globalData.avatar_fileid);
-              })
-          })
-      },
-      fail: err => {
-        console.log("openid获取失败");
-      }
+        success: res => {
+          const openid = res.result;
+          console.log("openid: " + openid);
+          this.globalData.openid = openid;
+                // 创建用户（保证用户已创建）
+          const data_createUser = {
+            "openid": openid,
+          };
+          this.callContainer("/api/v1/users", "POST", data_createUser)
+            .then( () => {
+              // console.log("用户创建成功");
+              // 查询用户信息
+              const data_getUserInfo = {
+                "openid": this.globalData.openid,
+              };
+              this.callContainer("/api/v1/users", "GET", data_getUserInfo)
+                .then(data => {
+                  // console.log("用户信息查询成功");
+                  // console.log(data);
+                  // 获取后，若有效，更新用户信息（全局变量）
+                  // （后续迭代：尽量从本地获取，减少请求量）
+                  if (data.nickname != "") {
+                    this.globalData.nickname = data.nickname;
+                  }
+                  if (data.avatar_fileid != "") {
+                    this.globalData.avatar_fileid = data.avatar_fileid;
+                  }
+                  // console.log(this.globalData.nickname);
+                  // console.log(this.globalData.avatar_fileid);
+                })
+            })
+          resolve(openid);
+        },
+        fail: err => {
+          console.log("openid获取失败");
+        },
+      });
     });
   },
 
@@ -87,7 +90,6 @@ App({
         },
 
         complete: res => {
-
           console.log("容器调用" + method + path 
                       + "返回状态码: " + res.statusCode.toString()); 
           // 经测试，toString将number转化为字符串
--- miniprogram/pages/mainPage/mainPage.js
+++ miniprogram/pages/mainPage/mainPage.js
@@ -1,77 +1,114 @@
 const app = getApp()
 Page({
-  // data: {
-  //   openid: "mainPage", // 测试用
-  //   userInfo: {
-  //     openid: null,
-  //     nickname: null,
-  //     avatarUrl: null,
-  //   },
+  data: {
+    videoList: [],
+    unitList: [],
+    loadingMtx: false,
+    refreshing: false, // 刷新状态
+  },
 
-  //   videoList: [],
-  //   endPoint: "/api/videos/url_list_by_user/",
-  //   loadingMtx: false,
-  //   refreshing: false, // 刷新状态
-  // },
-
-  // async onLoad(options) {
+  onLoad(options) {
   //   // wx.navigateTo({ url: "/pages/Top3Page/Top3Page", });
   //   console.log("[mainPage]页面加载");
-  //   this.getVideoList();
-  //   const res = await app.getUserInfo(this.data.openid);
-  //   this.setData({
-  //     userInfo: {
-  //       openid: res.openid,
-  //       nickname: res.nickname,
-  //       avatarUrl: res.avatar_url,
-  //     },
-  //   });
-  //   console.log("[mainPage:onLoad]Page userInfo.nickname: " + this.data.userInfo.nickname);
-  // },
+    app.globalData.openidPromise.then(openid => {
+      this.getUnitList();
+    });
+  },
 
-  // getVideoList(isRefresh = false) {
-  //   if (this.data.loadingMtx) return
-  //   console.log("[mainPage]开始getVideoList...");
-  //   this.setData({ loadingMtx: true }); // 上锁
-  //   // wx.request({
-  //   //   url: app.globalData.baseUrl + this.data.endPoint,
-  //   //   method: "GET",
-  //   //   data: {
-  //   //     openid: this.data.openid,
-  //   //   },
+  tapLike(event) {
+    const item = event.currentTarget.dataset.item;
+    console.log(item.is_liked);
+    console.log(item.index);
+    console.log(item);
+    console.log(this.data.unitList)
+    // 先承诺，再执行（确保正确执行）
+    // 先在前端响应
+    this.setData({
+      [`unitList[${item.index}].is_liked`]: !item.is_liked,
+    })
+    // 原来值变承诺值
+    item.flag = !item.is_liked;
+    // 再去尝试修改后端，按承诺方向执行
+    // 共同参数
+    const common_params = {
+      openid: app.globalData.openid,
+      fileid: item.video_fileid,
+    };
+    if (item.is_liked) {
+      app.callContainer("/api/v1/likes", "POST", common_params)
+    }
+    else {
+      app.callContainer("/api/v1/likes", "DELETE", common_params)
+    }
+  },
 
-  //   //   success: (res) => {
-  //   //     if(res.statusCode == 200) {
-  //   //       console.log("[mainPage]请求成功");
-  //   //       console.log(res);
-  //   //       this.setData({
-  //   //         videoList: this.data.videoList.concat(res.data.results),
-  //   //       });
-  //   //     }
-  //   //   },
-      
-  //   //   complete: () => {
-  //   //     this.setData({ loadingMtx: false });
-  //   //     console.log("[mainPage]请求结束 loadingMtx: " + this.data.loadingMtx);
-  //   //     console.log("[mainPage]videoList长度: " + this.data.videoList.length);
-  //   //     if(isRefresh) { this.setData({ refreshing: false, }); }
-  //   //   }
-  //   // })
-  // },
+  tapCollect(event) {
+    const item = event.currentTarget.dataset.item;
+    console.log(item);
+    // 先承诺，再执行（确保正确执行）
+    // 先在前端响应
+    this.setData({
+      [`unitList[${item.index}].is_collected`]: !item.is_collected,
+    })
+    // 原来值变承诺值
+    item.is_collected = !item.is_collected;
+    // 再去尝试修改后端，按承诺方向执行
+    // 共同参数
+    const common_params = {
+      openid: app.globalData.openid,
+      fileid: item.video_fileid,
+    };
+    if (item.is_collected) {
+      app.callContainer("/api/v1/collects", "POST", common_params)
+    }
+    else {
+      app.callContainer("/api/v1/collects", "DELETE", common_params)
+    }
+  },
 
-  // onReachBottom() {
-  //   console.log("[mainPage]触底了");
-  //   this.getVideoList();
-  // },
+  getUnitList(isRefresh = false) {
+    if (this.data.loadingMtx) return
+    this.setData({ loadingMtx: true }); // 上锁
 
-  // onRefresh() {
-  //   console.log("[mainPage]下拉刷新了");
-  //   this.setData({ refreshing: true });
-  //   this.setData({ videoList: [], });
-  //   this.getVideoList(true);
-    
-  //   console.log("[mainPage]onRefresh处理完了");
-  // },
+    // GET.Query参数，若想写数组：原本只能分开写
+    // JavaScript会合并同名键，wx.cloud.callContainer允许"键-数组"
+    const query_params = {
+      fileid_list: this.data.videoList,
+      need_count: 3,
+      openid: app.globalData.openid,
+    };
+    console.log("videoList传参:");
+    console.log(this.data.videoList);
+    app.callContainer("/api/v1/videos/random_n_with_userInfo", "GET", query_params)
+    .then(res => {
+      this.setData({
+        videoList: this.data.videoList.concat(res.fileid_list),
+        unitList: this.data.unitList.concat(res.unit_list)
+      });
+      console.log("获取视频后videoList:");
+      console.log(this.data.videoList);
+      console.log("获取视频后unitList:");
+      console.log(this.data.unitList);
+    })
+    .finally(() => {
+      this.setData({ loadingMtx: false });
+      if(isRefresh) { this.setData({ refreshing: false, }); }
+    })
+  },
+
+  onReachBottom() {
+    console.log("[mainPage]触底了");
+    this.getUnitList();
+  },
+
+  onRefresh() {
+    this.setData({ 
+      refreshing: true,
+      videoList: [], 
+      unitList: []
+    });
+    this.getUnitList(true);
+  },
 
   // onShareAppMessage() {
     
